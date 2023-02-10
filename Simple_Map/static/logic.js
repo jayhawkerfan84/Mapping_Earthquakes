e1b6e962e1382ebac25635d6416d86db92d32899
jayhawkerfan84 <adam.eastwood@gmail.com>
2023-02-10 10:41:35 -0600

    changes

--- Simple_Map/static/logic.js
+++ Simple_Map/static/logic.js
@@ -1,7 +1,7 @@
 console.log("this is working")
 
 //create the map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4)
+let map = L.map('mapid').setView([37.6214, -122.3790], 5);
 
 //create tile layer that will be the background for the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -25,3 +25,17 @@ cityData.forEach(city => {
         radius: city.population /100000
     }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map)
 })
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6214, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+ }).addTo(map);
