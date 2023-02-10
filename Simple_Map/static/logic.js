e5e37f987e95df028be41e7a13992b5a3bbb40b3
jayhawkerfan84 <adam.eastwood@gmail.com>
2023-02-10 11:49:00 -0600

    stopping

--- Simple_Map/static/logic.js
+++ Simple_Map/static/logic.js
@@ -3,8 +3,23 @@ console.log("this is working")
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }
+}).addTo(earthquakes);
+earthquakes.addTo(map)
 });
+
 
 //create the map object with a center and zoom level
 //let map = L.map('mapid').setView([30, 30], 2);
@@ -41,5 +56,72 @@ let map = L.map('mapid', {
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+//L.control.layers(baseMaps).addTo(map);
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
+
+  L.control.layers(baseMaps, overlays).addTo(map);
+
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+// Creating a GeoJSON layer with the retrieved data.
+// Creating a GeoJSON layer with the retrieved data.
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }
+}).addTo(map);
+    
+        function getColor(magnitude) {
+            if (magnitude > 5) {
+              return "#ea2c2c";
+            }
+            if (magnitude > 4) {
+              return "#ea822c";
+            }
+            if (magnitude > 3) {
+              return "#ee9c00";
+            }
+            if (magnitude > 2) {
+              return "#eecc00";
+            }
+            if (magnitude > 1) {
+              return "#d4ee00";
+            }
+            return "#98ee00";
+          }
