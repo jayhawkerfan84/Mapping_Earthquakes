d7908f7616b1a0360951e8528248795c66aa068b
jayhawkerfan84 <adam.eastwood@gmail.com>
2023-02-10 10:36:01 -0600

    stuff

--- Simple_Map/static/logic.js
+++ Simple_Map/static/logic.js
@@ -7,10 +7,21 @@ let map = L.map('mapid').setView([40.7, -94.5], 4)
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_Key
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
+//get data from cities
+let cityData = cities
+
+cityData.forEach(city => {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population /100000
+    }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map)
+})
